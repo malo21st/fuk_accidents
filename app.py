17b504d0a1e118ffb27af8274c47ea93d5c57d0e
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-02 13:21:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-
 weekdays = ['月', '火', '水', '木', '金', '土', '日']
 hours = [h for h in range(0, 23 + 1)]
 severity = ['負傷', '死亡']
@@ -24,28 +23,25 @@ def heatmap(df, weekdays):
 
 df = load_data()
 
-#body
-st.header("福岡県の交通事故")
-
+# sidebar
 select_days = st.sidebar.multiselect("曜日", weekdays, default=weekdays)
 
-start_hour, end_hour = st.sidebar.select_slider(
-    "時間帯", 
-    options=hours,
-    value=(0, 23),
-    )
+start_hour, end_hour = st.sidebar.select_slider("時間帯", options=hours, value=(0, 23))
 select_hours = [h for h in range(start_hour, end_hour + 1)]
 
 select_severity = st.sidebar.multiselect("事故内容", severity, default=severity)
 select_years = st.sidebar.multiselect("発生年", years, default=years)
 
+# data抽出
 df_hm = df[
-    df["発生曜日"].isin(select_days) &
-    df["発生時"].isin(select_hours) &
-    df["事故内容"].isin(select_severity) &
-    df["発生年"].isin(select_years)
-]
+            df["発生曜日"].isin(select_days) &
+            df["発生時"].isin(select_hours) &
+            df["事故内容"].isin(select_severity) &
+            df["発生年"].isin(select_years)
+        ]
 
+#body
+st.header("福岡県の交通事故")
 st.write(
     heatmap(df_hm, select_days)
 )
