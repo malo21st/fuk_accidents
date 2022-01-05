491f75eb6f1526eca50a62de8346aec5c78fffc6
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-06 06:32:37 +0900

    Update acc_map.py

--- acc_map.py
+++ acc_map.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+st.set_page_config(layout='wide')
+
 @st.cache
 def load_map_html(html_file):
     with open(html_file, mode="r") as f:
@@ -10,6 +12,5 @@ html_string = load_map_html("acc_map.html")
 
 #wrap all your code in this method and you should be done
 def app():
-#     st.set_page_config(layout='wide')
     components.html(html_string, width=900, height=600)
 #     components.html(html_string, width=700, height=500)
