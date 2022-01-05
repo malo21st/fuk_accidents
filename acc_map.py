d3d9e16322c20140e73d5959ccfd5f8059c3b541
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-06 06:33:09 +0900

    Update acc_map.py

--- acc_map.py
+++ acc_map.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(layout='wide')
+# st.set_page_config(layout='wide')
 
 @st.cache
 def load_map_html(html_file):
