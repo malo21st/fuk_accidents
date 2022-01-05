01a95fdb2f0941123d9ad81f7d24886e77690df1
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-06 05:20:10 +0900

    Update acc_map.py

--- acc_map.py
+++ acc_map.py
@@ -4,9 +4,9 @@ import streamlit.components.v1 as components
 @st.cache
 def load_map_html(html_file):
     with open(html_file, mode="r") as f:
-        return f.read("acc_map.html")
+        return f.read()
     
-html_string = load_map_html()
+html_string = load_map_html("acc_map.html")
 
 #wrap all your code in this method and you should be done
 def app():
