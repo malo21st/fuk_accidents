3bb8b0a1ba75bac8f8dee2507fbcfd4cbbae9214
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-07 06:26:34 +0900

    Update acc_app.py

--- acc_app.py
+++ acc_app.py
@@ -1,16 +1,27 @@
 import streamlit as st
 import acc_heatmap, acc_map #, acc_table
 from multipage import MultiPage
+import itertools
 
-# st.set_page_config(layout="wide")
+count = itertools.count()
 
-# Create an instance of the app 
-app = MultiPage()
+def set_mode(mode):
+    if next(count) == 0:
+        st.set_page_config(layout=mode)
+        print(f"*** set layout {mode} ***")
 
-# Add all your applications (pages) here
-app.add_page("発生時間帯", acc_heatmap.app)
-app.add_page("事故マップ", acc_map.app)
-# app.add_page("事故データ", acc_table.app)
+def main():
+    # Create an instance of the app 
+    app = MultiPage()
 
-# The main app
-app.run()
+    # Add all your applications (pages) here
+    app.add_page("発生時間帯", acc_heatmap.app)
+    app.add_page("事故マップ", acc_map.app)
+    # app.add_page("事故データ", acc_table.app)
+
+    # The main app
+    app.run()
+
+if __name__ == "__main__":
+    set_mode("wide")
+    main()
