39311d9e449f35d795630deacdc142a3f4d1d6c7
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-07 13:04:59 +0900

    Update acc_app.py

--- acc_app.py
+++ acc_app.py
@@ -2,16 +2,16 @@ import streamlit as st
 import acc_heatmap, acc_map #, acc_table
 from multipage import MultiPage
 import itertools
-from PIL import Image
+# from PIL import Image
 
 count = itertools.count()
 
 def set_mode(mode):
     if next(count) == 0:
-        im = Image.open("malo21st.png")
+#         im = Image.open("malo21st.png")
         st.set_page_config(
             page_title="福岡県の交通事故",
-            page_icon=im,
+#             page_icon=im,
             layout=mode,
         )
 
