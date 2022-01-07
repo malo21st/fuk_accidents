af7c753a3fba360d74ae7cd0a0ad00d943dc2030
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-07 13:01:10 +0900

    Update acc_app.py

--- acc_app.py
+++ acc_app.py
@@ -2,13 +2,18 @@ import streamlit as st
 import acc_heatmap, acc_map #, acc_table
 from multipage import MultiPage
 import itertools
+from PIL import Image
 
 count = itertools.count()
 
 def set_mode(mode):
     if next(count) == 0:
-        st.set_page_config(layout=mode)
-        print(f"*** set layout {mode} ***")
+        im = Image.open("malo21st.png")
+        st.set_page_config(
+            page_title="福岡県の交通事故",
+            page_icon=im,
+            layout=mode,
+        )
 
 def main():
     # Create an instance of the app 
