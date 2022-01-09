4048c99293c646506b7ee63fc47172248e26aa54
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-09 09:44:38 +0900

    Update acc_map.py

--- acc_map.py
+++ acc_map.py
@@ -37,7 +37,7 @@ def app():
     col1, col3, col4, col5 = st.columns((5, 1, 1, 3))
     with col1:
 #         element = st.info("", pos_index)
-        element = st.text_input("", value=pos_index)
+        element = st.text_input("")
 #     with col2:
 #         # st.write("　"); st.write("　");
 #         add_button = st.button('追加')
@@ -57,13 +57,14 @@ def app():
     if clear_button:
         pos_index = ""
 #         element.info(pos_index)
-        element.text_input("", value=pos_index)
+        element.text_input("")
 
     if inquiry_button:
         st.header('詳細情報')
         df_map = pd.DataFrame()
+        pos_index = element.text_input("")
         try:
-            pos_index = element.value
+            
             index_lst = list(itertools.chain.from_iterable([pos_dic[p] for p in set(pos_index.split())]))
             df_map = df[df.index.isin(index_lst)]
         except:
