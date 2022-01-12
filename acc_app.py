dd587de5fcc745f774ce97e11e9a5c607d8fe643
malo21st <44739330+malo21st@users.noreply.github.com>
2022-01-13 01:05:24 +0900

    Update acc_app.py

--- acc_app.py
+++ acc_app.py
@@ -6,8 +6,8 @@ from multipage import MultiPage
 st.session_state["is_first_time"] = True
 
 def set_mode(mode):
-#     im = Image.open("malo21st.png")
-    if not st.session_state.is_first_time:
+    if st.session_state.is_first_time:
+#         im = Image.open("malo21st.png")
         st.set_page_config(
             page_title="福岡県の交通事故",
             layout=mode,
